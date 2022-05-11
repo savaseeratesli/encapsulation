cdda7b84d57c51b3249b0ca0c8d8789ec2adb98d
Savaş Enes ERATEŞLİ <76652961+savaseeratesli@users.noreply.github.com>
2022-05-11 10:58:24 +0300

    Add files via upload

---
+++ encapsulation/encapsulation.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class calisan
+{
+private://Kapsüllendi,korundu
+    string isim = "null";
+    float maas;
+
+    //Private değerleri değiştirme ve erişmek için
+public:
+    string getIsim()
+    {
+        return isim;
+    }
+
+    void setIsim(string isim)
+    {
+        this->isim = isim;//this->isim private içindeki isim anlamında diğer isim ise bizim gönderdiğimiz
+    }
+
+};
+
+int main()
+{
+    calisan c1;
+
+    cout << c1.getIsim() << endl;
+
+    c1.setIsim("savasee");
+
+    cout << c1.getIsim() << endl;
+    
+}
